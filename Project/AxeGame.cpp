9c3f4afa23e0b647a8bd456be9cd07ee9531ba9b
Athar Siddiqui <atharsiddiqui211995@gmail.com>
2025-06-15 00:29:53 +0530

    .

---
+++ Project/AxeGame.cpp
@@ -0,0 +1,9 @@
+#include "raylib.h"
+int main(){
+    float rootbear = 1.99;
+    double cheeseburger =5.99;
+    bool shouldHavelunch {};
+    int width = 1000;
+    int height = 700;
+    InitWindow(width,height,"AxeGame");
+}
